896a62fba8080a651857238842e22d0bc624de0e
ptg0811 <ptg0811@gmail.com>
2023-08-26 01:40:16 +0900

    feat: add mysql

--- app copy.py
+++ app copy.py
@@ -1,21 +1,8 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
-import mysql.connector
-import os
 
 app = Flask(__name__)
-
-# MySQL 데이터베이스 연결 설정
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": os.environ.get("MYSQL_PASSWORD"),
-    "database": "irisDB"
-}
-
-db_connection = mysql.connector.connect(**db_config)
-db_cursor = db_connection.cursor()
 
 # 저장된 XGBoost 모델 불러오기
 model_filename = './app/model/iris_xgboost_model.joblib'
@@ -38,20 +25,12 @@ def predict():
     # 모델에 입력값 전달하여 예측 수행
     input_data = np.array(
         [[sepal_length, sepal_width, petal_length, petal_width]])
+
     predicted_class = loaded_model.predict(input_data)[0]
 
+    # 예측 결과를 문자로 변환하여 반환
     class_names = ["Setosa", "Versicolor", "Virginica"]
-
-    # 예측 결과를 숫자로 변환
     prediction_result = class_names[predicted_class]
-
-    # MySQL에 예측 결과 저장
-    sql_query = "INSERT INTO iris_data (sepal_length, sepal_width, petal_length, petal_width, class) VALUES (%s, %s, %s, %s, %s)"
-    values = (sepal_length, sepal_width, petal_length,
-              petal_width, int(predicted_class))
-
-    db_cursor.execute(sql_query, values)
-    db_connection.commit()
 
     return render_template('index.html', prediction_result=prediction_result)
 
